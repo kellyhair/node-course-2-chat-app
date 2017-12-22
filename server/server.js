d7f6f9da8121a08c6b56d90daf3f92c0f6cc84ec
Kelly Hair <kelly@routerlab.net>
2017-12-22 12:33:16 -0500

    Greet new user & alert others

--- server/server.js
+++ server/server.js
@@ -16,11 +16,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // socket.emit('newMessage', {
-  //   from: "mike@example.com",
-  //   text: "Hey, what up dog?",
-  //   createdAt: 123123
-  // });
+  socket.emit('newMessage', {
+    from: "Admin",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: "Admin",
+    text: "A new user has entered the chat",
+    createdAt: new Date().getTime()
+  });
+
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message)
@@ -29,6 +36,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+  //   socket.broadcast.emit('newMessage', {
+  //     from: message.from,
+  //     text: message.text,
+  //     createdAt: new Date().getTime()
+  //   });
   });
 
   socket.on('disconnect', () => {
@@ -36,9 +48,7 @@ io.on('connection', (socket) => {
 });
 
 });
-// app.get('/', (req, res) => {
-//
-// });
+
 
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
