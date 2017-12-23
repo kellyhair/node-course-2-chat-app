187842b6a8eac05a83dce54e70941b01ea771b49
Kelly Hair <kelly@routerlab.net>
2017-12-23 15:12:47 -0500

    added scroll functionality to the client side chat app

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 const socket = io();
 
+function scrollToBottom () {
+  // selectors
+
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  //heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  };
+};
+
 // socket.on('connect', () => { //arrow functions no bueno!
 socket.on('connect', function () {
   console.log('Connected to server');
@@ -24,6 +42,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -36,6 +55,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
